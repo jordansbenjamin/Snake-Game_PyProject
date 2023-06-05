e7b7e314b1569a5f87189baab4e747f32ea5e605
jordansbenjamin <14327@coderacademy.edu.au>
2023-06-06 09:30:52 +1000

    Created turtle objects

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title('Snake Game')
 
+xpos = 0
+
+for snake_body in range(3):
+    snake_body = Turtle('square')
+    snake_body.color('white')
+    snake_body.goto(x=xpos, y=0)
+    xpos += -20
 
 
 screen.exitonclick()

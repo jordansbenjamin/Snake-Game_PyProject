2eea3152da6d11e85f4568b7e0f3175ac662db15
jordansbenjamin <14327@coderacademy.edu.au>
2023-06-07 19:25:23 +1000

    Created Scoreboard class with score attributes and tally method + score object

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
-from scoreboard import Score
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -12,7 +12,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
-score = Score()
+score = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up,'Up')
@@ -31,5 +31,6 @@ while game_is_on:
     # Detect collision with food
     if snake.head.distance(food) < 15:
         food.new_location()
+        score.score_tally()
 
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -1,11 +1,16 @@
 from turtle import Turtle
 
-class Score(Turtle):
+class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
         self.color('white')
         self.penup()
         self.goto(0, 270)
-        self.write(arg=f'Score = {self.score}', align='center', font=('New Courier', 20, 'normal'))
+        self.write(arg=f'Score: {self.score}', align='center', font=('New Courier', 20, 'normal'))
         self.hideturtle()
+
+    def score_tally(self):
+        self.clear()
+        self.score += 1
+        self.write(arg=f'Score = {self.score}', align='center', font=('New Courier', 20, 'normal'))
